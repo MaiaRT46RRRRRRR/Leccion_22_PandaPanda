318184da8f357c515a0c8d6798059d3db1c65fbb
Maia <maia.rt.46@gmail.com>
2017-04-18 11:44:13 -0500

    Desarrollanado funcionalidad de img

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+var img =document.getElementsByClassName('ver');
+var cerrar =document.getElementsByClassName('cerrar');
+console.log(img);
+console.log(cerrar);
+
+  	var showModal=function(e){
+ 	 	this.parentNode.style.display="none";
+ 	}
+
+	for(var i=0;i<cerrar.length;i++){
+  		cerrar[i].addEventListener("click",showModal);
+  	}
+	
+	document.getElementById("resta").addEventListener("click",function(){
+		for(var i=0;i<img.length;i++){
+		img[i].style.display="inline-block"	;
+		}
+	});
